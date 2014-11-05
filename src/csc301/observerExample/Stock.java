634cbb4a4303f475b386ddcd93512cb0fb5efc09
Joey Freund <joey@cs.toronto.edu>
2014-11-05 17:36:36 -0500

    Work in progress ...

--- src/csc301/observerExample/Stock.java
+++ src/csc301/observerExample/Stock.java
@@ -1,28 +1,13 @@
 package csc301.observerExample;
 
 import java.math.BigDecimal;
+import java.util.Observable;
 
-public class Stock {
+public class Stock extends Observable {
 
 	
 	private String id;
 	private BigDecimal price;
-	
-	/*
-	 * A note about BigDecimal.
-	 * 
-	 * During exercise 1, some of you noticed that using double to save
-	 * currency values (limited to 2 decimal places), can lead to some
-	 * unexpected behaviour.
-	 * 
-	 * This is related to the way that doubles are stored in memory (64 bits,
-	 * interpreted as a binary number according to some IEEE standard) - Some numbers
-	 * cannot be represented accurately in binary, so we have some small error.
-	 * As we perform arithmetic operations (especially multiplication/division) these
-	 * errors propagate, and become more significant.
-	 * 
-	 * BigDecimal is a more suitable Java type for currency values.
-	 */
 	
 	
 	public Stock(String id, BigDecimal price) {
@@ -49,6 +34,7 @@ public class Stock {
 			throw new IllegalArgumentException("Price must be non-negative.");
 		}
 		this.price = price.setScale(2, BigDecimal.ROUND_HALF_UP);
+		this.setChanged();
 	}
 	
 	
